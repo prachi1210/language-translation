36397bc542fdc096a0f2a59cdf13a2fea6d876d0
adityathiru <T.Aditya@iiitb.org>
2017-06-12 19:20:40 +0530

    Forget Password functioning properly. Fixes #108

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -4,7 +4,7 @@ Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
   # confirmation, reset password and unlock tokens in the database.
- 
+
   config.secret_key = '3da5647d33e64804608ce4d3fa94497e9d04f8bd0f311259abd0cf8fec6b3e52c1bafd8c5bead8d7891792b328deaef0db2c51ea5939893d9fd22ab63f2a0262'
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
@@ -217,12 +217,12 @@ Devise.setup do |config|
   # config.unlock_in = 1.hour
 
   # Warn on the last attempt before the account is locked.
-  # config.last_attempt_warning = true 
+  # config.last_attempt_warning = true
 
   # ==> Configuration for :recoverable
   #
   # Defines which key will be used when recovering the password for an account
-  config.reset_password_keys = [ :username ]
+  config.reset_password_keys = [ :email ]
 
   # Time interval you can reset your password with a reset password key.
   # Don't put a too small interval or your users won't have the time to
