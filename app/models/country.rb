cb6786944e20b6c85c66d88691c89f3219df817f
adityathiru <T.Aditya@iiitb.org>
2017-06-19 12:09:30 +0530

    Fixes #102 - Country names are unique. Corresponding tests updated

--- app/models/country.rb
+++ app/models/country.rb
@@ -13,12 +13,13 @@
 class Country < ActiveRecord::Base
 include PublicActivity::Model
 tracked owner: Proc.new{ |controller, model| controller && controller.current_user }
- 
+
  belongs_to :organization
   belongs_to :user
   has_many :sites, dependent: :destroy
 
   validates_presence_of :name, :organization_id
+  validates_uniqueness_of :name, case_sensitive: false, scope: :organization_id
   # add search feature
     def self.search(search)
     where("name iLIKE ?", "%#{search}%")
